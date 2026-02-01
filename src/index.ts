d2e12c0e85296fa4ca50d89d143102efb71653df
netzumin <115604150+3aarl@users.noreply.github.com>
2026-02-01 21:38:26 +0800

    Clerk Setup
    # Progress Report
    ## Addition
    ### Clerk Express Backend
    - Addition of express for auth purposes
    ## Changes
    ### Updated Package.json & Package-lock.json
    - due to the addition of clerk
    ### Updated README and .env.example
    - added part of the new env requirements
    ### index,js test variable
    ## Removal
    - none

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-import { app } from "./server.ts"
 import { env } from "../env.ts"
+import { clerkMiddleware, clerkClient, requireAuth, getAuth } from "@clerk/express";
+import express, {response} from "express";
+
+const app = express();
+
+app.use(clerkMiddleware());
+
+// to test if clerk is connected to our backend, currently checks all users in the clerk dashboard
+// REMOVE WHEN DONE TESTING -k
+app.get("/", async (req, res) => {
+    const getUsers = await clerkClient.users.getUserList();
+
+    res.json(getUsers);
+})
 
 app.listen(env.PORT, () => {
-    console.log(`Server is running on http://localhost:${env.PORT}`)
-})
+    console.log(`Server is running on http://localhost:${env.PORT}`)})
