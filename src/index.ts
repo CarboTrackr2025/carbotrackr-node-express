0011417efbac8fcb9a80540bc557114ccd0f6a32
netzumin <115604150+3aarl@users.noreply.github.com>
2026-02-05 21:13:09 +0800

    Fetch User ID

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
 import { env } from "../env.ts"
-import { clerkMiddleware, clerkClient, requireAuth, getAuth } from "@clerk/express";
-import express, {response} from "express";
+import app from "./server.ts"
 
-const app = express();
-
-app.use(clerkMiddleware());
-
-// to test if clerk is connected to our backend, currently checks all users in the clerk dashboard
-// REMOVE WHEN DONE TESTING -k
-app.get("/", async (req, res) => {
-    const getUsers = await clerkClient.users.getUserList();
-
-    res.json(getUsers);
-})
-
+// Boot the API server (routes are mounted in server.ts, including /health)
 app.listen(env.PORT, () => {
-    console.log(`Server is running on http://localhost:${env.PORT}`)})
+    console.log(`Server is running on http://localhost:${env.PORT}`)
+})
